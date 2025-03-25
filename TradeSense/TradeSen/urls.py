ae2d8fbe6e1b9c01efcc788a106de1f52089d055
opensesame54 <saradhiaashrith@gmail.com>
2025-03-25 23:30:23 +0530

    "Updated Ver1"

--- TradeSense/TradeSen/urls.py
+++ TradeSense/TradeSen/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
 from . import views
-from .views import authView
+from .views import authView,home
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
-   path("signup/",authView,name="authView"),
-   path("accounts/",include("django.contrib.auth.urls"))
+   path("",home,name="home"),
+   path("signup/",authView,name="signup"),
+   path("accounts/",include("django.contrib.auth.urls")),
+   path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
 
--- TradeSense/TradeSen/views.py
+++ TradeSense/TradeSen/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+# Home View
+@login_required
+def home(request):
+    return render(request, "home.html", {})
+
+# Signup View
 def authView(request):
-    form=UserCreationForm()
-    return render(request,"registration/signup.html",{"form":form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  
